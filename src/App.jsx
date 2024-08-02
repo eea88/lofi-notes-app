548f094cb4fd9184c019ecd41e323f45f3e63062
Kim Delgado <kimdelgado1570@gmail.com>
2024-08-02 15:55:11 +0200

    create Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  
-
   return (
     <>
-     <h1>Hello World!</h1>
-     <Routes>
-     <Route path="/" element={<h2>Home Page</h2>} />
-     <Route path="/users/:userId/events/create" element={<h2>Create Event</h2>}></Route>
-     <Route path="/users/:userId/events/:eventId" element={<h2>Event Details</h2>}/>
-     <Route path="/users/:userId" element={<h2>User Page</h2>}/>
-     <Route path="/users/create" element={<h2>Create User</h2>}/>
-     <Route path="/about" element={<h2>About</h2>}/>
-     <Route path="*" element={<h2>Not Found</h2>}></Route>
-
-
-     </Routes>
+      <h1>Hello World!</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h2>Home Page</h2>} />
+        <Route
+          path="/users/:userId/events/create"
+          element={<h2>Create Event</h2>}
+        ></Route>
+        <Route
+          path="/users/:userId/events/:eventId"
+          element={<h2>Event Details</h2>}
+        />
+        <Route path="/users/:userId" element={<h2>User Page</h2>} />
+        <Route path="/users/create" element={<h2>Create User</h2>} />
+        <Route path="/about" element={<h2>About</h2>} />
+        <Route path="*" element={<h2>Not Found</h2>}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
