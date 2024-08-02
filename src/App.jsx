dc05b173f386492b1d88c614c7b6553dcceed422
Aquiles <Aquileshr92@gmail.com>
2024-08-02 15:17:02 +0200

    Router done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -8,6 +8,17 @@ function App() {
   return (
     <>
      <h1>Hello World!</h1>
+     <Routes>
+     <Route path="/" element={<h2>Home Page</h2>} />
+     <Route path="/users/:userId/events/create" element={<h2>Create Event</h2>}></Route>
+     <Route path="/users/:userId/events/:eventId" element={<h2>Event Details</h2>}/>
+     <Route path="/users/:userId" element={<h2>User Page</h2>}/>
+     <Route path="/users/create" element={<h2>Create User</h2>}/>
+     <Route path="/about" element={<h2>About</h2>}/>
+     <Route path="*" element={<h2>Not Found</h2>}></Route>
+
+
+     </Routes>
     </>
   )
 }
