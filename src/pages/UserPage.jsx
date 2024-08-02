865bf3e1383608f9aeeb54082ce5f8428fbf9f4c
Aquiles <Aquileshr92@gmail.com>
2024-08-02 17:57:20 +0200

    userPage - esqueleto

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,6 +1,31 @@
-import './UserPage.css'
+import "./UserPage.css";
+import { useState } from "react";
 
-function UserPage (){
+function UserPage() {
+  const [events, setEvents] = useState([3,5,7,3,2]);
+  return (
+    <section className="user-page-section">
+      <div className="create-event-container">
+        <button>Create Event</button>
+      </div>
+
+      <ul className="events-container">
+        {events.map((eachEvent) => {
+          return (
+            <li className="event-card">
+              <div>
+                <h2>Title of Event</h2>
+                <p>Participants</p>
+                <p>Date</p>
+                <p>Description</p>
+              </div>
+            </li>
+            
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
 
 export default UserPage;
