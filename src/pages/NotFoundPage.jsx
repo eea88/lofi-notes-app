99941faf67e87aefeb03399a10ae69302e6c9466
Kim Delgado <kimdelgado1570@gmail.com>
2024-08-03 18:01:28 +0200

    build notfound and add temporary styling

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom';
+import './NotFound.css'
+
+
 function NotFoundPage() {
     return (
-        <div>
+        <div className="not-found">
+            <div className="message-container">
             <h1>404</h1>
             <h3>The page you were trying to reach was</h3>
             <h2>NOT FOUND</h2>
+            </div>
+            <Link to="/">
+                <button></button>
+            </Link>
         </div>
     );
 }
