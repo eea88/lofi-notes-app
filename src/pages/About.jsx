44fb381f10936b609f3aaca983597ba55bc394b9
Kim Delgado <kimdelgado1570@gmail.com>
2024-08-03 21:15:04 +0200

    build about and set temporary styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,37 @@
 import './About.css'
 
 function About(){
-
+    return(
+        <section className="about">
+            <h1>Meet the developers</h1>
+            <div className="dev-container">
+                <img src="" alt="" />
+                <div className="dev-info">
+                    <h2>Esteban Escalante</h2>
+                    <p>blah blah blah</p>
+                    <p>add github here</p>
+                    <p>add linkedin here</p>
+                </div>
+            </div>
+            <div className="dev-container">
+                <img src="" alt="" />
+                <div className="dev-info">
+                    <h2>Aquiles Hinestrosa</h2>
+                    <p>blah blah blah</p>
+                    <p>add github here</p>
+                    <p>add linkedin here</p>
+                </div>
+            </div>
+            <div className="dev-container">
+                <img src="" alt="" />
+                <div className="dev-info">
+                    <h2>Kim Delgado</h2>
+                    <p>blah blah blah</p>
+                    <p>add github here</p>
+                    <p>add linkedin here</p>
+                </div>
+            </div>
+        </section>
+    )
 }
 export default About;
