77273ce4345fa504e6883d7d960bd70d3eef1354
Esteban Escalante <escalanteaveleyra@gmail.com>
2024-08-06 13:04:26 +0200

    Delete task done

--- src/components/WarningTask.jsx
+++ src/components/WarningTask.jsx
@@ -4,7 +4,7 @@ function WarningTask({ setShowTaskWarning, taskIdToDelete, deleteTask }) {
     return (
       <div className="warning-task-layout">
         <div className="warning-card">
-            <h3> Warning!</h3>
+            <h2> Warning!</h2>
           <p>Are you sure you want to delete this task?</p>
           {<button onClick={() =>deleteTask(taskIdToDelete)}>Yes</button> }
           <button onClick={() => setShowTaskWarning(false)}>No</button>
@@ -15,41 +15,10 @@ function WarningTask({ setShowTaskWarning, taskIdToDelete, deleteTask }) {
   
   export default WarningTask;
 
-  // things to add to the Event Details Page:
-
-  // const [showTaskWarning, setShowTaskWarning] = useState(false);
-  //const [taskIdToDelete, setTaskIdToDelete] = useState(null);
-
-  function deleteTask (id) {
-    //console.log("Attempting to delete event with ID:", id); // Debugging log
-    supabase
-      .from("tasks")
-      .delete()
-      .eq("id", id)
-      .then(
-        () => {setShowTaskWarning(false);
-          getTasks() } // need to review this line of code:
-      )
-      .catch((error) => console.error(error));
-  };
   
-  const displayTaskWarning = (id) => {
-    console.log("Display Task warning");
-    setShowTaskWarning(true);
-    setTaskIdToDelete(id);
-  };
 
-// things to add to the return html
 
-/* Review that the eachTask.id works
-<button onClick={() => displayTaskWarning(eachTask.id)}>❌</button> 
 
-{showTaskWarning && (
-    <WarningTask
-      deleteTask={deleteTask}
-      taskIdToDelete={taskIdToDelete}
-      setShowTaskWarning={setShowTaskWarning}
-    />
-  )}
-    */
+
+    
 
