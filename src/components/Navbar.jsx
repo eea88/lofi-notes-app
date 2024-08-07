afe8c461e274179ada42aa3c11c7ea3f93ce9e60
Aquiles <Aquileshr92@gmail.com>
2024-08-07 16:46:58 +0200

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 import './Navbar.css'
+import { useParams } from "react-router-dom";
 
 function Navbar() {
+    const session = JSON.parse(localStorage.getItem("sb-adznapocnpiuvpjbargm-auth-token"))
+
+    const userId = session.user.id
     return (
         <nav>
             <Link to="/">
                 <h1>Lofi Notes</h1>
             </Link>
-            <Link to="/users/:userId">
+            <Link to={`/users/${userId}`}>
                 <div className="user-icon">
                     <img src="" alt="" />
                 </div>
